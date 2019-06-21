34f327a40442246e57be43ffa7f609eab639b75b
Brian1011 <mutindabrian26@gmail.com>
2019-06-22 00:52:13 +0300

    save images encoded to the database

--- submitFaces.py
+++ submitFaces.py
@@ -22,18 +22,37 @@ thor = load_images("thor.jpeg")
 thompson_encoding = face_encoding(thompson)
 thor_encoding = face_encoding(thor)
 
+# convert image from numpy array to string
+x = thompson_encoding.dumps()
+
+# convert from string to numpy array
+y = np.loads(x)
+
+# Retrieve record from database
+mycursor = conn.mydb.cursor()
+mycursor.execute("SELECT * FROM `users` WHERE id=10")
+myresult = mycursor.fetchone()
+
+# Retrieve column with encoded image which is a blob
+thompson_encoding_db = myresult[3]
+print(np.array_equal(thompson_encoding, np.loads(thompson_encoding_db)))
+
 # save the details to the database
 """
-Since its an numpy array convert to string to save to database
-When fetched convert back to array
+Since its an numpy array convert to string to save to database --- x = thompson_encoding.tostring() 
+When fetched convert back to array --- np.fromstring(x, dtype=float) or np.loads(x)
 """
-mycursor = conn.mydb.cursor()
 
-# thompson
-# sql = "INSERT INTO `users` (`id`, `name`, `image_name`, `encoded_image`) VALUES (NULL, 'Thompson', 'thompson.jpeg', '[ss]')"
-print(type(thompson_encoding))
-x = thompson_encoding.tostring()
-print(len(x))
-print(np.fromstring(x, dtype=float))
-print(thompson_encoding)
-print(np.array_equal(np.fromstring(x, dtype=float), thompson_encoding))
+# save thompson
+sql = """INSERT INTO `users` (`name`, `image_name`, `encoded_image`) VALUES (%s, %s, %s)"""
+val = ('Thompson', 'thompson.jpeg', thompson_encoding.dumps())
+mycursor.execute(sql, val)
+conn.mydb.commit()
+print("Last record id", mycursor.lastrowid)
+
+# save thor
+sql = """INSERT INTO `users` (`name`, `image_name`, `encoded_image`) VALUES (%s, %s, %s)"""
+val = ('Thor', 'thor.jpeg', thor_encoding.tostring())
+mycursor.execute(sql, val)
+conn.mydb.commit()
+print("Thor record id", mycursor.lastrowid)
